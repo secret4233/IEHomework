da3467af07ed843e82de2ac6719f3d3d1471d607
陈炜超 <chenweichao.42@bytedance.com>
2020-09-05 22:19:40 +0800

    Initial Commit

---
+++ demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  demo
+//
+//  Created by bytedance on 2020/9/5.
+//  Copyright © 2020 secret42. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
