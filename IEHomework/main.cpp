f3100514ccba6492fc4a110e03abbb1e3e9e4eda
陈炜超 <chenweichao.42@bytedance.com>
2020-09-07 13:37:42 +0800

    有问题版本

--- IEHomework/main.cpp
+++ IEHomework/main.cpp
@@ -4,32 +4,37 @@
 using namespace cv;
 
 
-void colortransfer(Mat *mask,Mat image){ //蓝背景转白背景，有边缘残留
-    double Cb;
-    int num_row = image.rows,num_col = image.cols;
-    for (int r = 0; r < num_row; r++){
-        cv::Vec3b *data = image.ptr<cv::Vec3b>(r);
-        for (int c = 0; c < num_col; c++){
-            Cb = -0.169*data[c][2]  - 0.331*data[c][1] + 0.5*data[c][0]; //蓝色检测
-            //printf("%f ",Cb);
-            if (Cb > 5){//蓝色分量比GR分量的平均值高60且蓝色分量大于150
-                data[c][0] = 255;   //blue
-                data[c][1] = 255;   //green
-                data[c][2] = 255;   //red
+void getBackground(Mat image,Mat Mask){ //蓝背景转白背景，有边缘残留
+    int rowLen = image.rows,colLen = image.cols;
+    for (int r = 0; r < rowLen; r++){
+        Vec3b *data = image.ptr<Vec3b>(r);  //Vec3b:vector<uchar, 3>
+        Vec3b *mask = Mask.ptr<Vec3b>(r);
+        for (int c = 0; c < colLen; c++){//YCrCb
+            double Cb = -0.169*data[c][2]  - 0.331*data[c][1] + 0.5*data[c][0]; //蓝色检测
+            double Y= 0.299*data[c][2] + 0.587*data[c][1] + 0.114*data[c][0];
+            printf("%f ",Y);
+            if (Cb > 5){
+                mask[c][0] = mask[c][1] = mask[c][2] = 0;
             }
         }
-        //printf("\n");··
+        print("\n");
     }
 }
 
-
 int main(int argc, const char * argv[]) {
-    Mat image,mask;
-    image=imread("/Users/bytedance/Desktop/demo/opencv1.jpg",1);
-    mask = Mat::zeros(image.size(),CV_8UC1);
-    colortransfer(&mask,image);
+    Mat image,mask,imageAns;
+    image=imread("/Users/bytedance/Desktop/IEHomework/opencv1.jpg",1);
+    mask = Mat::zeros(image.size(),CV_8UC3);
+    mask.setTo(255);
+    getBackground(image,mask);
+
+    namedWindow("Mask Image",WINDOW_AUTOSIZE);
+    imshow("Mask Image", mask);
+    
+    bitwise_and(image, mask,imageAns);
     namedWindow("Display Image",WINDOW_AUTOSIZE);
-    imshow("Display Image", image);
+    imshow("Display Image", imageAns);
+    
     waitKey(0);
     return 0;
 }
