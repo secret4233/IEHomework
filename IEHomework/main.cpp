a81c60f87e294fc4a58a5c8c3155def0c0289795
陈炜超 <chenweichao.42@bytedance.com>
2020-09-12 22:06:33 +0800

    添加图片

--- IEHomework/main.cpp
+++ IEHomework/main.cpp
@@ -12,8 +12,6 @@ void getBackground(Mat Image,Mat Mask){
         for (int c = 0; c < colLen; c++){//YCrCb
             double Cb = -0.169*data[c][2] - 0.331*data[c][1] + 0.500*data[c][0]; //蓝色检测
             double Y  =  0.299*data[c][2] + 0.587*data[c][1] + 0.114*data[c][0]; //亮度检测
-            printf("%d %d %d ",data[c][2],data[c][1], data[c][0]);
-            printf("%f\n",Y);
             if (Cb > 5){
                 mask[c][0] = mask[c][1] = mask[c][2] = 0;
             }
@@ -52,12 +50,16 @@ int main(int argc, const char * argv[]) {
     
     namedWindow("Mask Image",WINDOW_AUTOSIZE);
     imshow("Mask Image", mask);
+    imwrite("/Users/bytedance/Desktop/IEHomework/mask.jpg",mask);
     
     namedWindow("Image",WINDOW_AUTOSIZE);
     imshow("Image", image);
+    imwrite("/Users/bytedance/Desktop/IEHomework/image.jpg",image);
     
     namedWindow("Display Image",WINDOW_AUTOSIZE);
     imshow("Display Image", imageAns);
+    imwrite("/Users/bytedance/Desktop/IEHomework/imageAns.jpg",imageAns);
+    
     
     waitKey(0);
     return 0;
